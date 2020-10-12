998e73bb1d7459c78eee55290d1534cc4827d3e2
eamiear <justcall_me_k@163.com>
2020-10-12 10:45:08 +0800

    download sb3 by dataurl

--- src/lib/download-blob.js
+++ src/lib/download-blob.js
@@ -1,3 +1,24 @@
+function getBlobDataUrl(blob) {
+    return new Promise(resolve => {
+        var blobUrl = URL.createObjectURL(blob);
+        var xhr = new XMLHttpRequest;
+        xhr.responseType = 'blob';
+        xhr.onload = function() {
+        var recoveredBlob = xhr.response;
+        var reader = new FileReader;
+        reader.onload = function() {
+            var blobAsDataUrl = reader.result;
+            console.log('url ==== ', blobAsDataUrl)
+            resolve(blobAsDataUrl)
+        };
+        reader.readAsDataURL(recoveredBlob);
+        };
+        xhr.open('GET', blobUrl);
+        xhr.send();
+    })
+
+  }
+
 export default (filename, blob) => {
     const downloadLink = document.createElement('a');
     document.body.appendChild(downloadLink);
@@ -7,8 +28,20 @@ export default (filename, blob) => {
         navigator.msSaveOrOpenBlob(blob, filename);
         return;
     }
+    const isPhone = (/Android|webOS|iPhone|iPod|BlackBerry|iPad/i.test(navigator.userAgent))
+    if (isPhone) {
+        return getBlobDataUrl(blob).then(url => {
+            downloadLink.href = url;
+            downloadLink.download = filename;
+            downloadLink.type = blob.type;
+            downloadLink.click();
+            window.URL.revokeObjectURL(url);
+            document.body.removeChild(downloadLink);
+        })
+    }
 
     const url = window.URL.createObjectURL(blob);
+    console.log('blob -=-=-= ', blob, url)
     downloadLink.href = url;
     downloadLink.download = filename;
     downloadLink.type = blob.type;
